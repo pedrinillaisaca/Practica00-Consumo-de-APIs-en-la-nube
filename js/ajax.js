e2dafdf48f5083f382c644f3b62a4a27f4775945
Pedro Illaisaca <pillaisaca@ups.edu.ec>
2020-04-06 23:13:34 -0500

    funcionalidad de paginacion completa

--- js/ajax.js
+++ js/ajax.js
@@ -1,49 +1,129 @@
-function buscarPeliculas() { 
- 
-    var nombre_pelicula=document.getElementById("peliculas").value;
-
-    console.log(nombre_pelicula);
-       
-  
-        if (window.XMLHttpRequest) { 
-            // code for IE7+, Firefox, Chrome, Opera, Safari 
-            xmlhttp = new XMLHttpRequest(); 
-        } else { 
-            // code for IE6, IE5 
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
-        } 
-        xmlhttp.onreadystatechange = function() { 
-            if (this.readyState == 4 && this.status == 200) { 
-                //alert("llegue"); 
-                /* document.getElementById("informacion").innerHTML = this.responseText;  */
-                cargarData(this.response);
-            } 
-        }; 
-        /* xmlhttp.open("GET","../../controladores/user/insertarInvitados.php?r_codigo="+r_codigo+"&u_codigo="+u_codigo,true);  */
-        xmlhttp.open("POST","http://www.omdbapi.com/?apikey=648317f1&s="+nombre_pelicula);
-        xmlhttp.send(); 
-        /* document.getElementById("tabladatos").disabled=true; */
-        //document.getElementById("anterior").disabled = true;
-    return false; 
-} 
-
-function cargarData(jsonString){    
+function buscarPeliculas() {
+    var nombre_pelicula = document.getElementById("peliculas").value;
     
-    var cadena=jsonString.substring(jsonString.indexOf("["),jsonString.indexOf("]")+1);
-    console.log(cadena);
-    var datos=JSON.parse(cadena);//convierte una cadena en objeto JSON
-    console.log(datos.length)
-    var detalle="";
-    for(var i=0; i<datos.length; i++){
-        console.log("entre al bucle")
-        detalle+="<tr>"+
-        "<td>"+datos[i].imdbID+"</td>"+
-        "<td>"+datos[i].Title+"</td>"+
-        "<td>"+datos[i].Year+"</td>"+       
-        "<td><img src="+datos[i].Poster+"></td>"+
-        "</tr>"; 
-        console.log(datos[i].Year);
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari 
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5 
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    document.getElementById("tabladatos").innerHTML=detalle;
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {            
+            /* document.getElementById("informacion").innerHTML = this.responseText;  */
+            cargarData(this.response);
+            
+        }
+    };
+    /* xmlhttp.open("GET","../../controladores/user/insertarInvitados.php?r_codigo="+r_codigo+"&u_codigo="+u_codigo,true);  */
+    xmlhttp.open("POST", "http://www.omdbapi.com/?apikey=648317f1&s=" + nombre_pelicula+"&r=json",true);
+    xmlhttp.send();
+    /* document.getElementById("tabladatos").disabled=true; */
+    //document.getElementById("anterior").disabled = true;
+    return false;
+}
+
+function cargarData(jsonString) {
+
+    var cadena = jsonString.substring(jsonString.indexOf("["), jsonString.indexOf("]") + 1);
+    /* var datos = JSON.parse(cadena);//convierte una cadena en objeto JSON  */
+    var code="<input type='text' id='objson' value='"+cadena+"' style='display: none;' />";/*  */
+    document.getElementById("informacion").innerHTML=code;
+    
+    return false;
+}
+
+
+function creadTable() {
+    /* var cadena = jsonString.substring(jsonString.indexOf("["), jsonString.indexOf("]") + 1); */
+    var cadena=document.getElementById("objson").value;
+    console.log(cadena);
+    var datos = JSON.parse(cadena);//convierte una cadena en objeto JSON
+
+    var detalle = "<table id='tablax' class='table table-striped table-bordered' style='width:100%'>"+
+              "<thead>"+
+                  "<th>DETALLES</th>"+
+                  "<th>NOMBRE</th>"+
+                  "<th>AÑO</th>"+
+                  "<th>POSTER</th>"+                  
+              "</thead>"+
+              "<tbody>";
+                
+              
+
+
+    for (var i = 0; i < datos.length; i++) {
+        detalle += "<tr>" +
+            "<td> <a id='" + datos[i].imdbID + "' onclick='ver_mas_detalles(this.id)' class='btn btn-more'>Ver mas</a></td>" + //onclick='ver_mas_detalles(this.innerText)'
+            "<td>" + datos[i].Title + "</td>" +
+            "<td>" + datos[i].Year + "</td>" +
+            "<td><img src=" + datos[i].Poster + "></td>" +
+            "</tr>";
+
+    }
+    document.getElementById("pincheTabla").innerHTML =detalle+"</tbody></table>";
+
+    $(document).ready(function () {
+        $('#tablax').DataTable({
+          language: {
+            processing: "Tratamiento en curso...",
+            search: "Buscar&nbsp;:",
+            lengthMenu: "Agrupar de _MENU_ items",
+            info: "Mostrando del item _START_ al _END_ de un total de _TOTAL_ items",
+            infoEmpty: "No existen datos.",
+            infoFiltered: "(filtrado de _MAX_ elementos en total)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron datos con tu busqueda",
+            emptyTable: "No hay datos disponibles en la tabla.",
+            paginate: {
+              first: "Primero",
+              previous: "Anterior",
+              next: "Siguiente",
+              last: "Ultimo"
+            },
+            aria: {
+              sortAscending: ": active para ordenar la columna en orden ascendente",
+              sortDescending: ": active para ordenar la columna en orden descendente"
+            }
+          },
+          scrollY: 400,
+          lengthMenu: [[5, 25, -1], [5, 25, "All"]],
+        });
+      });
+    return false;
+}
+
+
+function ver_mas_detalles(id_pelicula) {
+    //console.log("cod peliculeon es: ")
+
+    console.log(id_pelicula);
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari 
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5 
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //alert("llegue"); 
+            /* document.getElementById("informacion").innerHTML = this.responseText;  */
+            cargarInfPelicula(this.response);
+        }
+    };
+    /* xmlhttp.open("GET","../../controladores/user/insertarInvitados.php?r_codigo="+r_codigo+"&u_codigo="+u_codigo,true);  */
+    xmlhttp.open("POST", "http://www.omdbapi.com/?apikey=648317f1&i=" + id_pelicula + "&r=json&plot=full");
+    xmlhttp.send();
+    /* document.getElementById("tabladatos").disabled=true; */
+    //document.getElementById("anterior").disabled = true;
+    return false;
+}
+
+function cargarInfPelicula(jsonString) {
+    var datos = JSON.parse(jsonString);
+    console.log(typeof (datos));
+    console.log(datos);
 
 }
